066e63f9785ef4bb4d59ef07176126b44a3a0b21
stephaniee2 <stephaniejlouie@gmail.com>
2019-04-28 17:50:17 -0700

    modified for deployment

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
-const api_key = '1540b0816889c1455a1ddb998fe7801f'; 
-const resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json())
@@ -15,10 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/../dist/index.html'));
   });
   
-  app.get('/', function (req, res) {
-    res.send('GET request to the homepage')
-  })
 
-app.listen(3000, () => {
-    console.log('app listening on port 3000')
+app.listen(port, () => {
+    console.log('app listening on port ', port)
 })
