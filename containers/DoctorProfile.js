ef3d19f56415e23a581f1d5157eff1d4efe15793
stephaniee2 <stephaniejlouie@gmail.com>
2019-06-28 01:00:30 -0700

    selected doctor profile modal style edited

--- containers/DoctorProfile.js
+++ containers/DoctorProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import SimilarDoctors from "./SimilarDoctors";
 import "../dist/style.css";
-import Modal from 'react-responsive-modal';
+import Modal from "react-responsive-modal";
 
 class DoctorProfile extends Component {
   constructor(props) {
@@ -9,38 +9,47 @@ class DoctorProfile extends Component {
   }
 
   render() {
-    console.log(this.props.selectedDoctor)
     return (
-      <div className="modal-main">
-        <Modal open={this.props.open} onClose={this.props.onClose} animationDuration={500} closeOnOverlayClick={true} center>
-          <img src={this.props.selectedDoctor.profile.image_url} />
-          <br />
-          <div className="titleText">
-            {this.props.selectedDoctor.profile.first_name}{" "}
-            {this.props.selectedDoctor.profile.last_name}{" "}
-            {this.props.selectedDoctor.profile.title}
-          </div>
-          <br />
-          Specialties:
-          {this.props.selectedDoctor.specialties.map((specialty, i) => {
-            return (
-              <div key={i}>
-                <span className="titleText">{specialty.name}</span>
-                <br />
+      <div>
+        <Modal
+          open={this.props.open}
+          onClose={this.props.onClose}
+          animationDuration={500}
+          closeOnOverlayClick={true}
+          closeIconSize={15}
+          center
+        >
+          <div className="modal-info-container">
+            <div className="selected-doc-container">
+              <img
+                src={this.props.selectedDoctor.profile.image_url}
+                className="doctor-photo"
+              />
+              <div className="selected-doc-des">
+                <div className="doc-profile-txt">
+                  {this.props.selectedDoctor.profile.first_name}{" "}
+                  {this.props.selectedDoctor.profile.last_name}{" "}
+                  {this.props.selectedDoctor.profile.title}
+                </div>
+                <div className="doc-specialty">
+                  Specialties:
+                  {this.props.selectedDoctor.specialties.map((specialty, i) => {
+                    return (
+                      <div key={i}>
+                        <span className="titleText">{specialty.name}</span>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            );
-          })}
-          <br />
-          <br />
-          Doctors with similar specialties:
-          <SimilarDoctors
-            doctors={this.props.doctors}
-            selectedDoctor={this.props.selectedDoctor}
-          />
-          <br />
-          <button className="backButton" onClick={this.props.onClose}>
-            BACK TO LIST
-          </button>
+            </div>
+            Doctors with similar specialties:
+            <SimilarDoctors
+              doctors={this.props.doctors}
+              selectedDoctor={this.props.selectedDoctor}
+            />
+            <br />
+          </div>
         </Modal>
       </div>
     );
--- containers/SimilarDoctors.js
+++ containers/SimilarDoctors.js
@@ -7,34 +7,32 @@ class SimilarDoctors extends Component {
 
   render() {
     const filteredDoctors = this.props.doctors.filter(doctor => {
-      if(doctor.specialties.length > 0){
+      if (doctor.specialties.length > 0) {
         return (
           doctor.profile.last_name !==
             this.props.selectedDoctor.profile.last_name &&
           doctor.specialties[0].name ===
             this.props.selectedDoctor.specialties[0].name
         );
-      } 
-     
+      }
     });
 
     return (
-      <div>
+      <div className="similar-docs-container">
         {filteredDoctors.length > 0 ? (
           filteredDoctors.map((doctor, i, collection) =>
             !doctor.profile.image_url.includes("general_doctor_male") &&
-            !doctor.profile.image_url.includes("general_doctor_female") ? 
-              <div key={i}>
+            !doctor.profile.image_url.includes("general_doctor_female") ? (
+              <div className="doc-img" key={i}>
                 <img src={doctor.profile.image_url} />
                 <br />
                 {doctor.profile.first_name} {doctor.profile.last_name}{" "}
                 {doctor.profile.title}
-                <br />
-                <br />
+                
               </div>
-             : 
+            ) : (
               <div />
-            
+            )
           )
         ) : (
           <div>none</div>
