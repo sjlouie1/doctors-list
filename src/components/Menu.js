fcfb3310ffc237ada42d79a01b57a499f3f11def
Steven Chung <stevenchung.213@gmail.com>
2019-06-28 23:46:09 -0700

    added react-hot-reloader. express serving compressed assets now

---
+++ src/components/Menu.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const Menu = () => {
+  const categories = [
+    "Dashboard",
+    "Appointments",
+    "Calendar",
+    "Doctor Search",
+    "Payments",
+    "Messages",
+    "Settings"
+  ];
+
+  return (
+    <div className="menu">
+      <div className="patient-info">
+        {/* <img>LOGO HERE</img>
+        <img>PATINT IMG</img> */}
+        <p>online</p>
+      </div>
+      <div className="categories">
+        {categories.map((category, i) => {
+          return category === "Doctor Search" ? (
+            <div className="category" id="selected" key={"sidebar-" + i}>
+              {category}
+            </div>
+          ) : (
+            <div className="category" key={"sidebar-" + i}>
+              {category}{" "}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
