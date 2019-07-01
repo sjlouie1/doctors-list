b7a8364422dc3b4afc941e0f6086f9f598751127
stephaniee2 <stephaniejlouie@gmail.com>
2019-06-30 18:16:23 -0700

    styling added to doctor's list card

--- src/components/DoctorsList.js
+++ src/components/DoctorsList.js
@@ -2,62 +2,49 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DoctorsList = props => {
-
-  console.log(props.doctors)
-  const searchTypes = ['Name', 'Specialty'],
-    gender = ['Both', 'Male', 'Female'];
+  console.log(props.doctors);
+  const searchTypes = ["Name", "Specialty"],
+    gender = ["Both", "Male", "Female"];
   return (
     <div id="doctors-list-container">
       <div className="input-container">
         <form>
           <div className="inner-input-container">
-
             <input
               id="input-bar"
               name="input"
-              type='text'
+              type="text"
               value={props.search.input}
               onChange={props.handleInput}
               placeholder="Search..."
             />
 
-            <select
-              id="select-menu"
-              name="type"
-              onChange={props.handleInput}>
-              {
-                searchTypes.map((type) =>
-                  <option key={'option-' + type}>{type}</option>
-                )
-              }
+            <select id="select-menu" name="type" onChange={props.handleInput}>
+              {searchTypes.map(type => (
+                <option key={"option-" + type}>{type}</option>
+              ))}
             </select>
 
             <div id="radio-container">
-              {
-                gender.map(gender =>
-                  <label
-                    key={"radio-" + gender}
-                    className="radio-labels">
-                    <input
-                      name="gender"
-                      type="radio"
-                      value={gender}
-                      checked={props.search.gender === gender}
-                      onChange={props.handleInput}
-                    />
-                    {gender}
-                  </label>
-                )
-              }
+              {gender.map(gender => (
+                <label key={"radio-" + gender} className="radio-labels">
+                  <input
+                    name="gender"
+                    type="radio"
+                    value={gender}
+                    checked={props.search.gender === gender}
+                    onChange={props.handleInput}
+                  />
+                  {gender}
+                </label>
+              ))}
             </div>
-
           </div>
         </form>
       </div>
 
       <div className="list-container">
-        {
-          props.search.input === "" &&
+        {props.search.input === "" &&
           props.completeList.map((doctor, i) => {
             return (
               <div
@@ -65,38 +52,37 @@ const DoctorsList = props => {
                 className="doctor-card"
                 onClick={() => props.onClick(i)}
               >
-                <br/>
+                <br />
                 <div>
                   <img
                     className="doctor-photo"
                     src={doctor.profile.image_url}
                   />
                 </div>
-                <br/>
-                <div className="doc-description">
-                  <p className="titleText">{` ${doctor.profile.first_name} ${
-                    doctor.profile.last_name
+                <br />
+                <div className="doc-description-container">
+                  <div className="doc-description">
+                    <p className="titleText">{` ${doctor.profile.first_name} ${
+                      doctor.profile.last_name
                     } ${doctor.profile.title}`}</p>
+                  </div>
+                  <div>
+                    {doctor.specialties.map((specialty, i) => {
+                      return (
+                        <div key={i} className="doc-list-specialty">
+                          {/* <FontAwesomeIcon icon={"briefcase-medical"} /> */}
+                          <span>{specialty.name}</span>
+                          <br />
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-                <br/>
-                <div>
-                  <FontAwesomeIcon icon={"briefcase-medical"}/>
-                  {doctor.specialties.map((specialty, i) => {
-                    return (
-                      <div key={i} className="titleText">
-                        <span>{specialty.name}</span>
-                        <br/>
-                      </div>
-                    );
-                  })}
-                </div>
-                <br/>
               </div>
             );
-          })
-        }
-        {
-          props.search.input !== "" && props.doctors.length > 0 &&
+          })}
+        {props.search.input !== "" &&
+          props.doctors.length > 0 &&
           props.doctors.map((doctor, i) => {
             return (
               <div
@@ -104,42 +90,38 @@ const DoctorsList = props => {
                 className="doctor-card"
                 onClick={() => props.onClick(i)}
               >
-                <br/>
+                <br />
                 <div>
                   <img
                     className="doctor-photo"
                     src={doctor.profile.image_url}
                   />
                 </div>
-                <br/>
-                <div className="doc-description">
+                <br />
+                <div className="doc-name">
                   <p className="titleText">{` ${doctor.profile.first_name} ${
                     doctor.profile.last_name
-                    } ${doctor.profile.title}`}</p>
+                  } ${doctor.profile.title}`}</p>
                 </div>
-                <br/>
+                <br />
                 <div>
-                  <FontAwesomeIcon icon={"briefcase-medical"}/>
+                  <FontAwesomeIcon icon={"briefcase-medical"} />
                   {doctor.specialties.map((specialty, i) => {
                     return (
                       <div key={i} className="titleText">
                         <span>{specialty.name}</span>
-                        <br/>
+                        <br />
                       </div>
                     );
                   })}
                 </div>
-                <br/>
+                <br />
               </div>
             );
-          })
-        }
-        {
-          props.search.input !== "" && props.doctors.length === 0 &&
-          <h1>
-            No doctors matching the criteria of "{props.search.type}"
-          </h1>
-        }
+          })}
+        {props.search.input !== "" && props.doctors.length === 0 && (
+          <h1>No doctors matching the criteria of "{props.search.type}"</h1>
+        )}
       </div>
     </div>
   );
